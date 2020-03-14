1d87c4c56424ee18f3cd142aaeac7144fe72d391
Dexter Goh <bboifatt@gmail.com>
2020-03-14 16:45:20 +0800

    added content to about view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about', as: :about # allows me to use the path to about directly without using /pages path
+  get 'contact', to: 'pages#contact', as: :contact # allows me to use the path to about directly without using /pages path
+
+  root to: 'pages#home' # no idea what this does for me
+
+  get 'restaurants', to: 'restaurants#index'
 end
+
+
+
+
+
+
+
+
+  # get 'pages/contact' # GET REQUEST pages = ?? , /contact = ???
+  # get 'pages/about'
